87cdab0c5294eff3be3f387c9a147e4bdeaeb153
dejalltime <mart1449@algonquinlive.com>
2025-04-17 05:36:00 -0400

    replaced with event bus

--- plugins/messagequeue.js
+++ plugins/messagequeue.js
@@ -5,70 +5,32 @@ const fp = require('fastify-plugin')
 module.exports = fp(async function (fastify, opts) {
   fastify.decorate('sendMessage', function (message) {
     const body = message.toString()
-    if (process.env.ORDER_QUEUE_USERNAME && process.env.ORDER_QUEUE_PASSWORD) {
-      console.log('sending message ${body} to ${process.env.ORDER_QUEUE_NAME} on ${process.env.ORDER_QUEUE_HOSTNAME} using local auth credentials')
-      
-      const rhea = require('rhea')
-      const container = rhea.create_container()
-      var amqp_message = container.message;
 
-      const connectOptions = {
-        hostname: process.env.ORDER_QUEUE_HOSTNAME,
-        host: process.env.ORDER_QUEUE_HOSTNAME,
-        port: process.env.ORDER_QUEUE_PORT,
-        username: process.env.ORDER_QUEUE_USERNAME,
-        password: process.env.ORDER_QUEUE_PASSWORD,
-        reconnect_limit: process.env.ORDER_QUEUE_RECONNECT_LIMIT || 0
-      }
-      
-      if (process.env.ORDER_QUEUE_TRANSPORT !== undefined) {
-        connectOptions.transport = process.env.ORDER_QUEUE_TRANSPORT
-      }
-      
-      const connection = container.connect(connectOptions)
-      
-      container.once('sendable', function (context) {
-        const sender = context.sender;
-        sender.send({
-          body: amqp_message.data_section(Buffer.from(body,'utf8'))
-        });
-        sender.close();
-        connection.close();
-      })
+    const { ServiceBusClient } = require("@azure/service-bus");
 
-      connection.open_sender(process.env.ORDER_QUEUE_NAME)
-    } else if (process.env.USE_WORKLOAD_IDENTITY_AUTH === 'true') {
-      const { ServiceBusClient } = require("@azure/service-bus");
-      const { DefaultAzureCredential } = require("@azure/identity");
+    const connectionString = process.env.AZURE_SERVICEBUS_CONNECTION_STRING
+    const queueName = process.env.AZURE_SERVICEBUS_QUEUE
 
-      const fullyQualifiedNamespace = process.env.ORDER_QUEUE_HOSTNAME || process.env.AZURE_SERVICEBUS_FULLYQUALIFIEDNAMESPACE;
-
-      console.log(`sending message ${body} to ${process.env.ORDER_QUEUE_NAME} on ${fullyQualifiedNamespace} using Microsoft Entra ID Workload Identity credentials`);
-      
-      if (!fullyQualifiedNamespace) {
-        console.log('no hostname set for message queue. exiting.');
-        return;
-      }
-      
-      const queueName = process.env.ORDER_QUEUE_NAME
-
-      const credential = new DefaultAzureCredential();
-
-      async function sendMessage() {
-        const sbClient = new ServiceBusClient(fullyQualifiedNamespace, credential);
-        const sender = sbClient.createSender(queueName);
-
-        try {
-          await sender.sendMessages({ body: body });
-        } finally {
-          await sender.close();
-          await sbClient.close();
-        }
-      }
-      sendMessage().catch(console.error);
-    } else {
-      console.log('no credentials set for message queue. exiting.')
+    if (!connectionString || !queueName) {
+      console.log('Azure Service Bus connection string or queue name not defined. Exiting.')
       return
     }
+
+    async function sendMessage() {
+      const sbClient = new ServiceBusClient(connectionString);
+      const sender = sbClient.createSender(queueName);
+
+      try {
+        console.log(`Sending message "${body}" to queue "${queueName}"`);
+        await sender.sendMessages({ body: body });
+      } catch (err) {
+        console.error('Error sending message to Azure Service Bus:', err);
+      } finally {
+        await sender.close();
+        await sbClient.close();
+      }
+    }
+
+    sendMessage().catch(console.error);
   })
 })
